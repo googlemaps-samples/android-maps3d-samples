94c8aa054e5bf1cffc228b4ecbf3c4a1323adbc9
dkhawk <107309+dkhawk@users.noreply.github.com>
2026-03-17 10:39:30 -0600

    Address PR reviews: update copyrights and release Map3DView

--- Maps3DSamples/advanced/app/src/main/java/com/example/advancedmaps3dsamples/scenarios/RouteSampleActivity.kt
+++ Maps3DSamples/advanced/app/src/main/java/com/example/advancedmaps3dsamples/scenarios/RouteSampleActivity.kt
@@ -1,4 +1,4 @@
-// Copyright 2025 Google LLC
+// Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -125,7 +125,8 @@ class RouteSampleActivity : ComponentActivity() {
                                 })
                             },
                             onRelease = { view ->
-                                // Optional cleanup
+                                map3D = null
+                                view.onDestroy()
                             }
                         )
 
